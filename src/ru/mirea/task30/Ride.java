8ae0b8494054f523026ff710134f20ca83056a5c
Alabuzhev daniil <alabuzhev.danil@mail.ru>
2021-12-19 00:10:14 +0300

    1-32

---
+++ src/ru/mirea/task30/Ride.java
@@ -0,0 +1,27 @@
+package ru.mirea.task30;
+
+public class Ride {
+    private String brand;
+    private int year;
+
+    public Ride(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
